8a65e410d0c2ee79feefc6d21ed02f0feb168c05
Wolgrand Neto <wolgrand@live.com>
2020-04-13 10:55:59 -0300

    Challenge Finished

--- src/App.js
+++ src/App.js
@@ -1,29 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
-import "./styles.css";
+import './styles.css';
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository(e) {
+    e.preventDefault();
+
+    const data = {
+      url,
+      title,
+      techs,
+    };
+
+    const response = await api.post('repositories', data);
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(repository => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <form onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          placeholder="Url"
+          value={url}
+          onChange={e => setUrl(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Techs"
+          value={techs}
+          onChange={e => setTechs(e.target.value)}
+        />
+        />
+        <button type="submit">Adicionar</button>
+      </form>
     </div>
   );
 }
